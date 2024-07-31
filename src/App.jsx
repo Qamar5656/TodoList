66120d0188fcfebbfafee6bf97a3304420247ec0
Qamar5656 <qamar4157@gmail.com>
2024-07-31 23:41:13 +0500

    todolist

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoList from './components/TodoList.jsx'; // Updated path to match the folder structure
+import './components/TodoList.css'; // Uncommented the import statement and corrected the path
+
+function App() {
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
